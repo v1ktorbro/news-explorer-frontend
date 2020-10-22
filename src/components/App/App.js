52be0f694f34125f6972040524438b02a3383788
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-22 16:08:14 +0500

    refactor: удалить import шапки из App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import React from 'react';
-import Header from '../Header/Header';
 import Main from '../Main/Main';
 
 function App() {
   return (
     <>
-      <Header />
       <Main />
     </>
   );
