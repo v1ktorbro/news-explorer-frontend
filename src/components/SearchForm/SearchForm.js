be9c565ff287b6ded0d48a636ef985eb9e390b87
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-24 23:50:52 +0500

    style: переписать имена классов через kebab-case

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 function SearchForm() {
   return (
-    <form className="searchForm__form">
-      <input type="text" className="searchForm__input" placeholder="Введите тему новости" />
-      <button type="button" className="searchForm__btn-search">Искать</button>
+    <form className="search-form__form">
+      <input type="text" className="search-form__input" placeholder="Введите тему новости" />
+      <button type="button" className="search-form__btn-search">Искать</button>
     </form>
   );
 }
