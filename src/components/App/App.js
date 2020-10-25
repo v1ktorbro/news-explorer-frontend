397ba1f05e305197d988fb6e74a2fdc9e0a38c2e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-25 18:04:08 +0500

    fix: добавить тени и указатель при hover

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
+import NewsCardList from '../NewsCardList/NewsCardList';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <NewsCardList />
       <About />
       <Footer />
     </>
