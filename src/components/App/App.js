99a08b60edb3efcf8ccf42bec22871d107113bba
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-29 17:30:53 +0500

    удалить импорт попапа из App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,10 @@ import Main from '../Main/Main';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 import NewsCardList from '../NewsCardList/NewsCardList';
-import PopupWithForm from '../PopupWithForm/PopupWithForm';
 
 function App() {
   return (
     <>
-      <PopupWithForm />
       <Header />
       <Main />
       <NewsCardList />
