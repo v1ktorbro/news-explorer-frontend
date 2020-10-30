309880790b8c81f7eeb2c51906ed4fcb4337616e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-10-30 16:57:56 +0500

    отрисовывать компонент в зависимости от пропса

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logoutImage from '../../images/logout.svg';
 
-function Navigation() {
+function Navigation({ onMain, loggedIn }) {
   return (
     <nav className="navigation">
-      <NavLink to="/" className="navigation__link navigation__link_active" activeClassName="">Главная</NavLink>
-      <NavLink to="/" className="navigation__link" activeClassName="">Сохранённые статьи</NavLink>
-      <button type="button" name="logout" className="navigation__btn-logout">
-        Витёк
-        <img className="navigation__image-logout" alt="картинка logout" src={logoutImage} />
+      <NavLink to="/main" className={onMain ? 'navigation__link navigation__link_main' : 'navigation__link'} activeClassName={onMain ? 'navigation__link_active-main' : 'navigation__link_active'}>Главная</NavLink>
+      { loggedIn && <NavLink to="/saved-news" className={onMain ? 'navigation__link navigation__link_main' : 'navigation__link'} activeClassName={onMain ? 'navigation__link_active-main' : 'navigation__link_active'}>Сохранённые статьи</NavLink>}
+      <button type="button" name="logout" className={loggedIn ? 'navigation__btn-logout' : 'navigation__btn-login'}>
+        { loggedIn ? 'Витёк' : 'Авторизоваться'}
+        { loggedIn && <img className="navigation__image-logout" alt="картинка logout" src={logoutImage} />}
       </button>
     </nav>
   );
