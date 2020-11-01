7aad48e52651229e18153ce3e8ba42d478bd4038
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-02 00:13:30 +0500

    добавить элементы прелоудару в случае ошибки поиска новостей

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,11 +1,16 @@
 import './Preloader.css';
 import React from 'react';
 
-function Preloader() {
+function Preloader({ success }) {
   return (
-    <section className="preloader preloader_closed">
-      <span className="preloader__circle" />
-      <span className="preloader__title">Идет поиск новостей...</span>
+    <section className="preloader">
+      <span className={success ? 'preloader__circle' : 'preloader__error-result'} />
+      {!success && <h2 className="preloader__title">Ничего не найдено</h2>}
+      <span
+        className={success ? 'preloader__description' : 'preloader__description preloader__description_error-result'}
+      >
+        {success ? 'Идет поиск новостей...' : 'К сожалению по вашему запросу ничего не найдено.'}
+      </span>
     </section>
   );
 }
