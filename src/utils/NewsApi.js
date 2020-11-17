6dca344d8680c3004847fbc293cb9c950b0d00e7
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-17 15:18:08 +0500

    refactor(NewsApi): метод getResultSearch должен возвращать в любом случае ответ в формате json

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -10,12 +10,7 @@ class NewsApi {
   getResultSearch(reqWords) {
     const today = new Date();
     return fetch(`${this.url}?q=${reqWords}&apiKey=${this.apiKey}&from=${today.getDate() - 7}&to=${today.getDate()}&pageSize=100`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Что-то пошло не так: ошибка ${res.status}`);
-      });
+      .then((res) => res.json());
   }
 }
 
