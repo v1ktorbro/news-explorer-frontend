59c116acc58a66585935cd276c414e036eefa24c
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-17 15:14:37 +0500

    refactor(NewsCardList): добавить пропс отвечающий за ошибку при поиске новостей

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -2,16 +2,21 @@ import './NewsCardList.css';
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 
-function NewsCardList() {
+function NewsCardList({ searchWithError }) {
   return (
     <section className="news-card-list">
       <h2 className="news-card-list__title">Результаты поиска</h2>
-      <div className="news-card-list__cards">
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-      </div>
-      <button type="button" className="news-card-list__btn-show-more">Показать еще</button>
+      { searchWithError
+        ? <p className="news-card-list__description-error">Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.</p> : (
+          <>
+            <div className="news-card-list__cards">
+              <NewsCard />
+              <NewsCard />
+              <NewsCard />
+            </div>
+            <button type="button" className="news-card-list__btn-show-more">Показать еще</button>
+          </>
+        )}
     </section>
   );
 }
