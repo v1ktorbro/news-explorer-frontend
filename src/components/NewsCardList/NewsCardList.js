165ef2386dafaa533d67a8a477d1b1e1376b8a48
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-18 05:53:01 +0500

    feat(NewsCardList): кнопка «Показать ещё» должна пропасть, когда все карточки отрисованы

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -4,6 +4,15 @@ import NewsCard from '../NewsCard/NewsCard';
 
 function NewsCardList({ searchWithError, cards }) {
   const [loadIndexCards, setIsLoadIndexCard] = React.useState(3);
+
+  const showMoreCards = () => {
+    const btnShowMore = document.querySelector('.news-card-list__btn-show-more');
+    setIsLoadIndexCard(loadIndexCards + 3);
+    if (cards.length <= loadIndexCards + 3) {
+      btnShowMore.classList.add('news-card-list__btn-show-more_closed');
+    }
+  };
+
   return (
     <section className="news-card-list">
       <h2 className="news-card-list__title">Результаты поиска</h2>
@@ -19,7 +28,7 @@ function NewsCardList({ searchWithError, cards }) {
                 />
               ))}
             </div>
-            <button type="button" onClick={() => setIsLoadIndexCard(loadIndexCards + 3)} className="news-card-list__btn-show-more">Показать еще</button>
+            <button type="button" onClick={showMoreCards} className="news-card-list__btn-show-more">Показать еще</button>
           </>
         )}
     </section>
