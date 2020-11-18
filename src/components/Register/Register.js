add038de15af9409ac0de309d001d02103ae60cc
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-19 01:35:35 +0500

    refactor(Register): собрать введенные данные в рефы и отправить их в основной компонент App

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,16 +4,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 
-function Register({ isOpen, onClose, onLogin }) {
+function Register({
+  isOpen, onClose, onLogin, handleRegister,
+}) {
+  const emailInputRef = React.useRef();
+  const passwordInputRef = React.useRef();
+  const nameInputRef = React.useRef();
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    handleRegister({
+      email: emailInputRef.current.value,
+      password: passwordInputRef.current.value,
+      name: nameInputRef.current.value,
+    });
+  };
+
   return (
-    <PopupWithForm name="register" title="Регистрация" isOpen={isOpen} onClose={onClose}>
+    <PopupWithForm name="register" onSubmit={onSubmit} title="Регистрация" isOpen={isOpen} onClose={onClose}>
       <span className="login__signature-input login__signature-input_email">Email</span>
-      <input type="email" className="login__input login__input_email" placeholder="Введите почту" />
+      <input ref={emailInputRef} type="email" required className="login__input login__input_email" placeholder="Введите почту" />
       <span className="login__signature-input login__signature-input_password">Пароль</span>
-      <input type="password" className="login__input login__input_email" placeholder="Введите пароль" />
+      <input ref={passwordInputRef} type="password" required minLength="4" className="login__input login__input_email" placeholder="Введите пароль" />
       <span className="login__signature-input login__signature-input_name">Имя</span>
-      <input type="text" className="login__input login__input_name" placeholder="Введите свое имя" />
-      <button type="submit" disabled className="login__btn-submit">Зарегистрироваться</button>
+      <input ref={nameInputRef} type="text" required minLength="2" className="login__input login__input_name" placeholder="Введите свое имя" />
+      <button type="submit" className="login__btn-submit">Зарегистрироваться</button>
       <div className="login__block-link">
         <span className="login__issue">
           или
