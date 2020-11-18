14fbb8671d7a5a170ae0bac2c485c83866053785
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-18 23:40:30 +0500

    feat(MainApi): создать класс и методы для сохранения и удаления статей

---
+++ src/utils/MainApi.js
@@ -0,0 +1,41 @@
+/* eslint-disable arrow-body-style */
+class MainApi {
+  constructor({ url, headers }) {
+    this.url = url;
+    this.headers = headers;
+  }
+
+  saveArticle(dataOfArticle) {
+    return fetch(`${this.url}/articles`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: this.headers,
+      body: JSON.stringify({
+        keyword: dataOfArticle.source.id,
+        title: dataOfArticle.title,
+        text: dataOfArticle.description,
+        date: dataOfArticle.publishedAt,
+        source: dataOfArticle.source,
+        link: dataOfArticle.url,
+        image: dataOfArticle.urlToImage,
+      }),
+    }).then((res) => {
+      return res.json();
+    });
+  }
+
+  deleteArticle(articleId) {
+    return fetch(`${this.url}/articles/${articleId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: this.headers,
+    });
+  }
+}
+
+const mainApi = new MainApi({
+  url: 'http://localhost:3001',
+  headers: '',
+});
+
+export default mainApi;
