c85c20304c5d518b078a9adad478c0c2787cb520
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-20 11:18:39 +0500

    fix(Navigation): картинка logout цветом отображается в зависимости от пропса

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import './Navigation.css';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logoutImage from '../../images/logout.svg';
+import logoutImageBlackColor from '../../images/logout_black.svg';
+import logoutImageWhiteColor from '../../images/logout_white.svg';
 import CurrentUserContext from '../../context/CurrentUserContext';
 
 function Navigation({
-  onMain, loggedIn, onLogin,
+  onMain, loggedIn, onLogin, savedNews,
 }) {
   const currentUser = React.useContext(CurrentUserContext);
 
@@ -13,9 +14,9 @@ function Navigation({
     <nav className="navigation">
       <NavLink exact to="/" className={onMain ? 'navigation__link navigation__link_main' : 'navigation__link'} activeClassName={onMain ? 'navigation__link_active-main' : 'navigation__link_active'}>Главная</NavLink>
       { loggedIn && <NavLink to="/saved-news" className={onMain ? 'navigation__link navigation__link_main' : 'navigation__link'} activeClassName={onMain ? 'navigation__link_active-main' : 'navigation__link_active'}>Сохранённые статьи</NavLink>}
-      <button type="button" name="logout" onClick={onLogin} className={loggedIn ? 'navigation__btn-logout' : 'navigation__btn-login'}>
+      <button type="button" name="logout" onClick={onLogin} className={loggedIn && savedNews ? 'navigation__btn-logout' : 'navigation__btn-login'}>
         { loggedIn ? currentUser.name : 'Авторизоваться'}
-        { loggedIn && <img className="navigation__image-logout" alt="картинка logout" src={logoutImage} />}
+        { loggedIn && <img className="navigation__image-logout" alt="картинка logout" src={savedNews ? logoutImageBlackColor : logoutImageWhiteColor} />}
       </button>
     </nav>
   );
