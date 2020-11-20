386ca5ed3b584c9518b2b21ffb242ed5ac457149
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-20 14:46:52 +0500

    fix(NewsCard): удалить карточку отрабатывает корректно

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -5,7 +5,7 @@ import './NewsCard.css';
 import React from 'react';
 
 function NewsCard({
-  savedCard, card, loggedIn, onArticleSave,
+  savedCard, card, loggedIn, onArticleSave, onArticleDelete,
 }) {
   const dateCreateNews = (dateISO8601) => new Intl.DateTimeFormat('ru', {
     year: 'numeric',
@@ -37,13 +37,17 @@ function NewsCard({
     hitnElement.classList.toggle('news-card__hint-login_closed');
   };
 
+  const deleteSavedCard = () => {
+    onArticleDelete(card._id);
+  };
+
   return (
     <div className="news-card">
       <div className="news-card__block-image">
-        <img src={card.urlToImage} className="news-card__image-main" onClick={() => window.open(card.url)} alt="фото карточки" />
+        <img src={savedCard ? card.image : card.urlToImage} className="news-card__image-main" onClick={() => window.open(card.url)} alt="фото карточки" />
         { savedCard && (
         <div className="news-card__keyword-block">
-          <span className="news-card__keyword-text">Природа</span>
+          <span className="news-card__keyword-text">{card.keyword}</span>
         </div>
         ) }
         <div
@@ -51,14 +55,14 @@ function NewsCard({
           onMouseEnter={(evt) => hintShow(evt)}
           onMouseLeave={(evt) => hintHide(evt)}
         >
-          <button onClick={(evt) => { savedCardNews(evt); }} className={savedCard ? 'news-card__btn-delete' : 'news-card__btn-bookmark'} aria-label="bookmark" type="button" />
+          <button onClick={savedCard ? deleteSavedCard : (evt) => { savedCardNews(evt); }} className={savedCard ? 'news-card__btn-delete' : 'news-card__btn-bookmark'} aria-label="bookmark" type="button" />
           <div className="news-card__hint-login news-card__hint-login_closed">
             <span className="news-card__text-hint">{ savedCard ? 'Убрать из сохранённых' : 'Войдите,  чтобы сохранять статьи' }</span>
           </div>
         </div>
       </div>
       <div className="news-card__info-block" onClick={() => window.open(card.url)}>
-        <span className="news-card__date">{dateCreateNews(card.publishedAt)}</span>
+        <span className="news-card__date">{savedCard ? dateCreateNews(card.date) : dateCreateNews(card.publishedAt)}</span>
         <h3 className="news-card__title">{card.title}</h3>
         <p className="news-card__description">{card.description}</p>
         <span className="news-card__source">{card.source.name}</span>
