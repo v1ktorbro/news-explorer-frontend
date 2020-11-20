26b7972285bf3bca32479c2089e87d285e8cde7e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2020-11-20 14:45:22 +0500

    fix(SavedNews): секция подгружает информацию о карточек со стейта переданного из App

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -2,15 +2,18 @@ import './SavedNews.css';
 import React from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 
-function SavedNews() {
+function SavedNews({ cards, onArticleDelete }) {
   return (
     <section className="saved-news">
       <div className="saved-news__cards">
-        <NewsCard savedCard />
-        <NewsCard savedCard />
-        <NewsCard savedCard />
-        <NewsCard savedCard />
-        <NewsCard savedCard />
+        {cards.map((card) => (
+          <NewsCard
+            savedCard
+            key={card._id}
+            card={card}
+            onArticleDelete={onArticleDelete}
+          />
+        ))}
       </div>
     </section>
   );
